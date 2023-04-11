93736125a183f256b780f0ee19cb00477f2ace7a
Aditya Kumar <aryan68125@gmail.com>
2023-04-11 13:38:17 +0530

    first commit iteration question added

---
+++ Iteration_control_structure/iteration_control_structure_excercise2.java
@@ -0,0 +1,14 @@
+class Tester {
+	public static void main(String[] args) {
+		// Implement your code here
+		int a = 8;
+		int num = 1;
+		System.out.print("1 ");
+		for(int i =0;i<a-1;i++)
+		{
+		  num = num*2;
+		  System.out.print(num+" ");
+		}
+		System.out.println("");
+	}
+}
